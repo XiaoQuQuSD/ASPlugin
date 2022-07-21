21e27313dcd0c17cca210b3bdba576e8e6e4f385
XiaoQuQu <1107439216@qq.com>
2022-07-21 12:23:52 +0800

    Add WayPoint

--- src/main/java/ASPlugin/commands/AddWayPoint.java
+++ src/main/java/ASPlugin/commands/AddWayPoint.java
@@ -3,10 +3,14 @@ package ASPlugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class AddWayPoint implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command com, String label, String[] args) {
+        if (!(sender instanceof Player))
+            return false;
+        
         return false;
     }
 }
